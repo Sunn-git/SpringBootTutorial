d6b2b622c9bf2b1c374d25664b896802f4a7702b
geranium236 <geranium236@gmail.com>
2021-03-25 12:04:38 +0900

    Project Setting.

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@GetMapping
+	public List<String> hello() {
+		List<String> list = new ArrayList<String>();
+		list.add("Hello");
+		list.add("World");
+		return list;
+	}
+
+}
