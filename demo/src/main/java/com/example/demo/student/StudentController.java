aa56515500f4960fab4400098a72148f0f816068
geranium236 <geranium236@gmail.com>
2021-03-25 17:25:59 +0900

    [Deleting Students] Delete a student by id when the id exists

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -3,7 +3,9 @@ package com.example.demo.student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +30,11 @@ public class StudentController {
 	@PostMapping
 	public void registerNewStudent(@RequestBody Student student) { //RequestBody에서 student 객체를 받아온다
 		studentService.addNewStudent(student);
+	}
+	
+	@DeleteMapping(path = "{studentId}")
+	public void deleteStudent(@PathVariable("studentId") Long studentId) {
+		studentService.deleteStudent(studentId);
 	}
 	
 }
